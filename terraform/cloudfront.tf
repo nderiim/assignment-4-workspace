0a8fa72d7c11fa432dc5f9cae42ff8a0dee0e93f
Nderim Bunjaku <bunjaku.nderim@hotmail.com>
2023-07-19 20:17:36 +0200

    updated cloudfront

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,3 +1,12 @@
+resource "aws_cloudfront_origin_access_control" "web_client" {
+  name                              = "tali_web_client_oac"
+  description                       = "Grant access to s3 media bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+
 module "web_client_cdn" {
   source  = "terraform-aws-modules/cloudfront/aws"
   version = "3.2.1"
@@ -8,39 +17,27 @@ module "web_client_cdn" {
   is_ipv6_enabled = true
   price_class     = "PriceClass_All"
 
-  create_origin_access_identity = true
-  origin_access_identities = {
-    s3_web_client = "Web Client Bucket"
-  }
+default_root_object = "index.html"
 
+  create_origin_access_identity = false
 
-  origin = {
+ origin = {
     web_client = {
-      domain_name = "nderim.assignment-4.appstellar.training"
-      custom_origin_config = {
-        http_port              = 80
-        https_port             = 443
-        origin_protocol_policy = "match-viewer"
-        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-      }
-    }
-
-    s3_web_client = {
-      domain_name = module.web_client.s3_bucket_bucket_domain_name
-      s3_origin_config = {
-        origin_access_identity = "s3_web_client"
-      }
+      domain_name              = module.web_client.s3_bucket_bucket_domain_name
+      origin_access_control_id = aws_cloudfront_origin_access_control.web_client.id
     }
   }
 
-  default_cache_behavior = {
-    target_origin_id           = "web_client"
-    viewer_protocol_policy     = "allow-all"
+default_cache_behavior = {
+    target_origin_id       = "web_client"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
 
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
-    compress        = true
-    query_string    = true
+    min_ttl     = 1
+    default_ttl = 86400
+    max_ttl     = 31536000
   }
 
 
